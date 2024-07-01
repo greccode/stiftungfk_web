75168b03c8af2384ce7247012714eb6aaa9dad68
muhammedoenal@outlook.com <Moes?!1997>
2024-07-01 23:41:08 +0200

    Stiftungsziele

--- FK-Stiftung/Controllers/HomeController.cs
+++ FK-Stiftung/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace FK_Stiftung.Controllers
             return View();
         }
 
+        public IActionResult Impressum()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ FK-Stiftung/Controllers/Stiftungsziele.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FK_Stiftung.Controllers
+{
+    public class Stiftungsziele : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
