36645348e20a20d30cf3af3d342ce03a006d781d
Leon Oet <loet@stud.fra-uas.de>
2024-07-01 17:22:47 +0200

    edit im projectcontroller hinzugefügt und view erstellt.

--- FK-Stiftung/Controllers/ProjectController.cs
+++ FK-Stiftung/Controllers/ProjectController.cs
@@ -38,5 +38,44 @@ namespace FK_Stiftung.Controllers
             }
             return View();
         }
+
+        // pause
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            { 
+                return NotFound();
+            }
+           
+            Project? projectFromDb = _db.Projects.Find(id);
+            // Other ways to retrieve data from the database
+           //  Project? projectFromDb1 = _db.Projects.FirstOrDefault(u => u.Id == id);
+          //   Project? projectFromDb2 = _db.Projects.Where(u => u.Id == id).FirstOrDefault();
+
+            if (projectFromDb == null) 
+            {
+                return NotFound();
+            }
+            return View(projectFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Project obj)
+        {
+            //server-side validation
+            if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("name", "Name und Beschreibung können nicht exakt gleich sein");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Projects.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
