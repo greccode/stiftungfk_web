e4395433c6b696a6fc46342d25a468adb7369946
muhammedoenal@outlook.com <Moes?!1997>
2024-07-01 17:05:48 +0200

    Home-Page aktualisiert

---
+++ FK-Stiftung/Controllers/OnlinemagazinController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FK_Stiftung.Controllers
+{
+    public class OnlinemagazinController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
