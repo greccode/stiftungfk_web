8610dbea3d888d2fde68c750728e72bb370e024a
Leon Oet <loet@stud.fra-uas.de>
2024-07-04 17:35:42 +0200

    ExtendedUserIdentity(Fix)

---
+++ FK-Stiftung/Data/Migrations/20240704153350_TestMigrationAfterDbFix.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FK_Stiftung.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestMigrationAfterDbFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PicturePath",
+                value: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=205119&type=card");
+
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "PicturePath",
+                value: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=205119&type=card");
+
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "PicturePath",
+                value: "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=205119&type=card");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PicturePath",
+                value: "C:\\Users\\leono\\source\\repos\\FK-Stiftung\\FK-Stiftung\\wwwroot\\images\\buch_offen_mit_schuber.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "PicturePath",
+                value: "C:\\Users\\leono\\source\\repos\\FK-Stiftung\\FK-Stiftung\\wwwroot\\images\\buch_offen_mit_schuber.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "PicturePath",
+                value: "C:\\Users\\leono\\source\\repos\\FK-Stiftung\\FK-Stiftung\\wwwroot\\images\\buch_offen_mit_schuber.jpg");
+        }
+    }
+}
